4febc439e4b0cb915440ae53ab2665d92001cb7b
John Furtado <jf8846@gmail.com>
2017-06-30 18:56:41 +0000

    added error message and styling, form fields reset

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+/*global $*/
+
 $(document).ready(function() {
     
-    $("#user-survey").submit(validation);
+    $('#user-survey').submit(validation);
     
 });
 
@@ -9,15 +11,39 @@ function validation(event) {
     //prevent form submission
     event.preventDefault();
     
-    var email = $("#email").val();
-    var verify = $("#verify").val();
+    //clear error
+    clearError();
     
-    if (email != verify) {
-        $("#error").text("no match");
+    var password = $('#password').val();
+    var verify = $('#verify').val();
+    
+    //if passwords do not match display error
+    if (password != verify) {
+        displayError();   
     }
+    //else clear error and alert user
     else {
-        alert("User info submitted");    
+        clearError();
+        alert("User info submitted"); 
+        $('#user-survey').trigger("reset");
     }
 
     
-};
+}
+
+function displayError() {
+    $('.error-message').attr("hidden", false);
+    $('.error-message').text(" Password does not match.");
+    $('#password').attr('style', "border: 1px solid #9C4D49; color: #9C4D49; background-color: #EFDFDF");
+    $('#verify').attr('style', "border: 1px solid #9C4D49; color: #9C4D49; background-color: #EFDFDF");
+    $('label[for=password]').attr('style', "color: #9C4D49");
+    $('label[for=verify]').attr('style', "color: #9C4D49");
+}
+
+function clearError() {
+    $(".error-message").text("");
+    $('#password').attr('style', "");
+    $('#verify').attr('style', "");
+    $('label[for=password]').attr('style', "");
+    $('label[for=verify]').attr('style', "");
+}
